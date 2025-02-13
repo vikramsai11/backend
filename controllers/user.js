270e80194cfe915a20ece00f073818e85216c55a
vikramsai10 <vikramsaithota@gmail.com>
2025-02-13 09:39:52 +0530

    added files

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ const path = require("path");
 const User = require("../model/user");
 const router = express.Router();
 const {upload} = require("../multer");
-const ErrorHandler = require("../utils/errorhandler");
+const ErrorHandler = require("../utils/ErrorHandler");
 
 // create user
 router.post("/create-user",upload.single("file"), async(req,res)=>{
